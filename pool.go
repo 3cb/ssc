a7cf750a32511fa4f001159ab763e037fc33dd4d
Marc C-B <marc.cbrooks@gmail.com>
2017-11-10 12:44:26 -0500

    move Socket type definitions and methods into separate file

--- pool.go
+++ pool.go
@@ -101,75 +101,8 @@ func (p *SocketPool) Control() {
 	}
 }
 
-// Socket type defines a websocket connection
-type Socket struct {
-	URL          string
-	Connection   *websocket.Conn
-	isConnected  bool
-	isReadable   bool
-	isWritable   bool
-	ClosingIndex int
-	OpenedAt     time.Time
-	ClosedAt     time.Time
-}
 
-func (s *Socket) Read(shutdown <-chan string, errorChan chan<- ErrorMsg, pipe Pipe) {
-	defer func() {
-		err := s.Connection.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-		if err != nil {
-			log.Printf("Closing (%v) at %v\n: %v", s.URL, time.Now(), err)
-		}
-		s.Connection.Close()
-	}()
-	for {
-		select {
-		case url := <-shutdown:
-			if url == s.URL {
-				log.Printf("Shutdown message received from controller(%v).\n", url)
-				errorChan <- ErrorMsg{s, nil}
-				return
-			}
-		default:
-			readType, msg, err := s.Connection.ReadMessage()
-			if err != nil {
-				log.Printf("Error reading from websocket(%v): %v\n", s.URL, err)
-				errorChan <- ErrorMsg{s, err}
-				return
-			}
-			pipe.ToPool <- Data{s, readType, msg}
-		}
-	}
-}
 
-func (s *Socket) Write(shutdown <-chan string, errorChan chan<- ErrorMsg, pipe Pipe) {
-	defer func() {
-		err := s.Connection.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-		if err != nil {
-			log.Printf("Closing (%v) at %v\n: %v", s.URL, time.Now(), err)
-		}
-		s.Connection.Close()
-	}()
-	for {
-		select {
-		case url := <-shutdown:
-			if url == s.URL {
-				log.Printf("Shutdown message received from controller(%v).\n", url)
-				errorChan <- ErrorMsg{s, nil}
-				return
-			}
-		default:
-			msg := <-pipe.FromPool
-			if msg.Socket.URL == s.URL {
-				err := s.Connection.WriteMessage(msg.Type, msg.Payload)
-				if err != nil {
-					log.Printf("Error writing to websocket(%v): %v\n", s.URL, err)
-					errorChan <- ErrorMsg{s, err}
-					return
-				}
-			}
-		}
-	}
-}
 
 // Pipe contains two data communication channels: inbound and outbound
 type Pipe struct {
@@ -203,42 +136,6 @@ func (s *Socket) Connect(errorChan chan<- ErrorMsg, shutdown <-chan string, pipe
 	s.isConnected = true
 	s.OpenedAt = time.Now()
 
-	// Start goroutine to listen to websocket.
-	// Closes connection and sends error message on error.
-	// If shutdown message is received websocket error message is sent with nil error value and socket is Closed.
-	go func() {
-		defer func() {
-			s.Connection.Close()
-			s.ClosedAt = time.Now()
-			s.isConnected = false
-		}()
-
-		for {
-			select {
-			case url := <-shutdown:
-				if url == s.URL {
-					log.Printf("Close Message Received from Controller. Closing websocket at: %v", s.URL)
-					errorChan <- ErrorMsg{s, nil}
-					return
-				}
-			case f := <-pipe.FromPool:
-				err := f.Socket.Connection.WriteMessage(f.Type, f.Payload)
-				if err != nil {
-					log.Printf("Error writing to websocket: %v", f.Socket.URL)
-					errorChan <- ErrorMsg{f.Socket, err}
-					return
-				}
-			default:
-				readType, msg, err := s.Connection.ReadMessage()
-				if err != nil {
-					log.Printf("Error reading from websocket(%v): ", err)
-					errorChan <- ErrorMsg{s, err}
-					return
-				}
-				pipe.ToPool <- Data{s, readType, msg}
-			}
-		}
-	}()
 
 	return true
 }
---
+++ socket.go
@@ -0,0 +1,69 @@
+// Socket type defines a websocket connection
+type Socket struct {
+	URL          string
+	Connection   *websocket.Conn
+	isConnected  bool
+	isReadable   bool
+	isWritable   bool
+	ClosingIndex int
+	OpenedAt     time.Time
+	ClosedAt     time.Time
+}
+
+func (s *Socket) Read(shutdown <-chan string, errorChan chan<- ErrorMsg, pipe Pipe) {
+	defer func() {
+		err := s.Connection.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+		if err != nil {
+			log.Printf("Closing (%v) at %v\n: %v", s.URL, time.Now(), err)
+		}
+		s.Connection.Close()
+	}()
+	for {
+		select {
+		case url := <-shutdown:
+			if url == s.URL {
+				log.Printf("Shutdown message received from controller(%v).\n", url)
+				errorChan <- ErrorMsg{s, nil}
+				return
+			}
+		default:
+			readType, msg, err := s.Connection.ReadMessage()
+			if err != nil {
+				log.Printf("Error reading from websocket(%v): %v\n", s.URL, err)
+				errorChan <- ErrorMsg{s, err}
+				return
+			}
+			pipe.ToPool <- Data{s, readType, msg}
+		}
+	}
+}
+
+func (s *Socket) Write(shutdown <-chan string, errorChan chan<- ErrorMsg, pipe Pipe) {
+	defer func() {
+		err := s.Connection.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+		if err != nil {
+			log.Printf("Closing (%v) at %v\n: %v", s.URL, time.Now(), err)
+		}
+		s.Connection.Close()
+	}()
+	for {
+		select {
+		case url := <-shutdown:
+			if url == s.URL {
+				log.Printf("Shutdown message received from controller(%v).\n", url)
+				errorChan <- ErrorMsg{s, nil}
+				return
+			}
+		default:
+			msg := <-pipe.FromPool
+			if msg.Socket.URL == s.URL {
+				err := s.Connection.WriteMessage(msg.Type, msg.Payload)
+				if err != nil {
+					log.Printf("Error writing to websocket(%v): %v\n", s.URL, err)
+					errorChan <- ErrorMsg{s, err}
+					return
+				}
+			}
+		}
+	}
+}
