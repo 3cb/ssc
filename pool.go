c851093291578181d9a19eddcf8192aa25971253
Marc C-B <marc.cbrooks@gmail.com>
2017-11-10 12:36:43 -0500

    add Read() and Write() methods to *Socket type

--- pool.go
+++ pool.go
@@ -1,30 +1,30 @@
 package ssc
 
 import (
+	"errors"
 	"log"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-// SocketPool is a collection of websocket connections combined with 3 channels used to send and received message to and from the goroutines that control them
+// SocketPool is a collection of websocket connections combined with 4 channels which are used to send and received messages to and from the goroutines that control them
 type SocketPool struct {
 	Stack        map[string]*Socket
 	Pipe         Pipe // {ToPool chan Data, FromPool chan Data}
 	ErrorChan    chan ErrorMsg
-	ShutdownChan chan *Socket
-	OpenQueue    []*Socket
-	ClosingStack   map[string]bool
+	ShutdownChan chan string
+	ClosingStack map[string]bool
 }
 
 // NewSocketPool creates a new instance of SocketPool and returns a pointer to it
 func NewSocketPool(urls []string, readable bool, writable bool) (*SocketPool, error) {
 	if readable == false && writable == false {
-		err := errors.New("Bad input values: Sockets cannot be both unreadable and unwritable.")
+		err := errors.New("bad input values: Sockets cannot be both unreadable and unwritable")
 		return nil, err
 	}
 	errorChan := make(chan ErrorMsg)
-	shutdown := make(chan *Socket)
+	shutdown := make(chan string)
 	toPool := make(chan Data)
 	fromPool := make(chan Data)
 	pipe := Pipe{toPool, fromPool}
@@ -34,21 +34,21 @@ func NewSocketPool(urls []string, readable bool, writable bool) (*SocketPool, er
 		Pipe:         pipe,
 		ErrorChan:    errorChan,
 		ShutdownChan: shutdown,
-		ClosingStack:   map[string]bool,
+		ClosingStack: make(map[string]bool, len(urls)),
 	}
 
 	for _, v := range urls {
 		index := 0
 		if readable == true {
-			index += 1
+			index++
 		}
 		if writable == true {
-			index += 1
+			index++
 		}
 		s := &Socket{
-			URL:        v,
-			isReadable: readable,
-			isWritable: writable,
+			URL:          v,
+			isReadable:   readable,
+			isWritable:   writable,
 			ClosingIndex: index,
 		}
 		success := s.Connect(errorChan, shutdown, pipe)
@@ -74,17 +74,17 @@ func (p *SocketPool) Control() {
 					e.Socket.isConnected = false
 					e.Socket.ClosedAt = time.Now()
 					delete(p.Stack, e.Socket.URL)
-					log.Printf("Websocket at %v has been closed.")
+					log.Printf("Websocket at %v has been closed.", e.Socket.URL)
 				} else {
 					// at to closing stack send shutdown message
 				}
-				p.ShutdownChan <- e.Socket
+				p.ShutdownChan <- e.Socket.URL
 			case e.Error == nil && e.Socket.ClosingIndex == 1:
 				e.Socket.Connection.Close()
 				e.Socket.isConnected = false
 				e.Socket.ClosedAt = time.Now()
 				delete(p.Stack, e.Socket.URL)
-				log.Printf("Websocket at %v has been closed.")
+				log.Printf("Websocket at %v has been closed.", e.Socket.URL)
 			case e.Error == nil && e.Socket.ClosingIndex == 2:
 				if p.ClosingStack[e.Socket.URL] == false {
 					p.ClosingStack[e.Socket.URL] = true
@@ -94,41 +94,45 @@ func (p *SocketPool) Control() {
 					e.Socket.ClosedAt = time.Now()
 					delete(p.ClosingStack, e.Socket.URL)
 					delete(p.Stack, e.Socket.URL)
-					log.Printf("Websocket at %v has been closed.")
+					log.Printf("Websocket at %v has been closed.", e.Socket.URL)
 				}
 			}
 		}
-	{
+	}
 }
+
 // Socket type defines a websocket connection
 type Socket struct {
-	URL         string
-	Connection  *websocket.Conn
-	isConnected bool
-	isReadable  bool
-	isWritable  bool
+	URL          string
+	Connection   *websocket.Conn
+	isConnected  bool
+	isReadable   bool
+	isWritable   bool
 	ClosingIndex int
-	// Close       chan int // Receives close messages from Read/Write goroutines
-	// CloseMsg    int      // counts number of close messages from Read/Write goroutines. Closes Socket at 2
-	OpenedAt time.Time
-	ClosedAt time.Time
+	OpenedAt     time.Time
+	ClosedAt     time.Time
 }
 
-func (s *Socket) Read(shutdown <-chan *Socket, errorChan chan<- ErrorMsg, pipe Pipe) {
+func (s *Socket) Read(shutdown <-chan string, errorChan chan<- ErrorMsg, pipe Pipe) {
 	defer func() {
-
+		err := s.Connection.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+		if err != nil {
+			log.Printf("Closing (%v) at %v\n: %v", s.URL, time.Now(), err)
+		}
+		s.Connection.Close()
 	}()
 	for {
 		select {
-		case m := <-shutdown:
-			if m.Connection == s.Connection {
+		case url := <-shutdown:
+			if url == s.URL {
+				log.Printf("Shutdown message received from controller(%v).\n", url)
 				errorChan <- ErrorMsg{s, nil}
 				return
 			}
 		default:
 			readType, msg, err := s.Connection.ReadMessage()
 			if err != nil {
-				log.Printf("Error reading from websocket(%v): ", err)
+				log.Printf("Error reading from websocket(%v): %v\n", s.URL, err)
 				errorChan <- ErrorMsg{s, err}
 				return
 			}
@@ -137,8 +141,34 @@ func (s *Socket) Read(shutdown <-chan *Socket, errorChan chan<- ErrorMsg, pipe P
 	}
 }
 
-func (s *Socket) Write(shutdown <-chan *Socket, errorChan chan<- ErrorMsg, pipe Pipe) {
-
+func (s *Socket) Write(shutdown <-chan string, errorChan chan<- ErrorMsg, pipe Pipe) {
+	defer func() {
+		err := s.Connection.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+		if err != nil {
+			log.Printf("Closing (%v) at %v\n: %v", s.URL, time.Now(), err)
+		}
+		s.Connection.Close()
+	}()
+	for {
+		select {
+		case url := <-shutdown:
+			if url == s.URL {
+				log.Printf("Shutdown message received from controller(%v).\n", url)
+				errorChan <- ErrorMsg{s, nil}
+				return
+			}
+		default:
+			msg := <-pipe.FromPool
+			if msg.Socket.URL == s.URL {
+				err := s.Connection.WriteMessage(msg.Type, msg.Payload)
+				if err != nil {
+					log.Printf("Error writing to websocket(%v): %v\n", s.URL, err)
+					errorChan <- ErrorMsg{s, err}
+					return
+				}
+			}
+		}
+	}
 }
 
 // Pipe contains two data communication channels: inbound and outbound
@@ -162,7 +192,7 @@ type ErrorMsg struct {
 
 // Connect connects to websocket given a url string and three channels from SocketPool type.
 // Creates a goroutine to receive and send data as well as to listen for errors and calls to shutdown
-func (s *Socket) Connect(errorChan chan<- ErrorMsg, shutdown <-chan *Socket, pipe Pipe) bool {
+func (s *Socket) Connect(errorChan chan<- ErrorMsg, shutdown <-chan string, pipe Pipe) bool {
 	c, _, err := websocket.DefaultDialer.Dial(s.URL, nil)
 	if err != nil {
 		log.Printf("Error connecting to websocket: \n%v\n%v", s.URL, err)
@@ -185,8 +215,8 @@ func (s *Socket) Connect(errorChan chan<- ErrorMsg, shutdown <-chan *Socket, pip
 
 		for {
 			select {
-			case sock := <-shutdown:
-				if sock.Connection == s.Connection {
+			case url := <-shutdown:
+				if url == s.URL {
 					log.Printf("Close Message Received from Controller. Closing websocket at: %v", s.URL)
 					errorChan <- ErrorMsg{s, nil}
 					return
