2189eaf50cc2a440a4589c51678084cd955534ba
Marc C-B <marc.cbrooks@gmail.com>
2017-12-07 18:08:06 -0500

    split Control() method into ControlRead(), ControlWrite(), and ControlShutdown()

--- pool.go
+++ pool.go
@@ -20,14 +20,17 @@ type SocketPool struct {
 // Pipes contains data communication channels:
 // Error channel carries websocket error messages from goroutines back to pool controller
 type Pipes struct {
-	InboundBytes    chan Data
-	InboundJSON     chan JSONReaderWriter
-	OutboundBytes   chan Data
-	OutboundJSON    chan JSONReaderWriter
-	FromSocketBytes chan Data
-	FromSocketJSON  chan JSONReaderWriter
-	ErrorRead       chan ErrorMsg
-	ErrorWrite      chan ErrorMsg
+	InboundBytes        chan Data
+	InboundJSON         chan JSONReaderWriter
+	OutboundBytes       chan Data
+	OutboundJSON        chan JSONReaderWriter
+	StopControlRead     chan struct{}
+	StopControlWrite    chan struct{}
+	StopControlShutdown chan struct{}
+	FromSocketBytes     chan Data
+	FromSocketJSON      chan JSONReaderWriter
+	ErrorRead           chan ErrorMsg
+	ErrorWrite          chan ErrorMsg
 }
 
 // PoolConfig is used to pass configuration settings to the Pool initialization function
@@ -102,11 +105,90 @@ func NewSocketPool(urls []string, config PoolConfig) (*SocketPool, error) {
 	return pool, nil
 }
 
-// Control method listens for Error Messages and dispatches shutdown messages
-// It also routes Data messages to and from websockets
+// Control method launches ControlShutdown(), ControlRead(), and ControlWrite()
 func (p *SocketPool) Control() {
+	go p.ControlShutdown()
+	if p.Config.IsReadable {
+		go p.ControlRead()
+	}
+	if p.Config.IsWritable {
+		go p.ControlWrite()
+	}
+}
+
+// ControlRead runs an infinite loop to take messages from websocket servers and send them to the outbound channel
+func (p *SocketPool) ControlRead() {
+	defer func() {
+		log.Printf("ControlRead goroutine was stopped at %v.\n", time.Now())
+	}()
+	if p.Config.IsJSON {
+		for {
+			select {
+			case <-p.Pipes.StopControlRead:
+				return
+			case v := <-p.Pipes.FromSocketJSON:
+				p.Pipes.OutboundJSON <- v
+			default:
+				continue
+			}
+		}
+	} else {
+		for {
+			select {
+			case <-p.Pipes.StopControlRead:
+				return
+			case v := <-p.Pipes.FromSocketBytes:
+				p.Pipes.OutboundBytes <- v
+			default:
+				continue
+			}
+		}
+	}
+}
+
+// ControlWrite runs an infinite loop to take messages from inbound channel and send to write goroutines
+func (p *SocketPool) ControlWrite() {
+	defer func() {
+		log.Printf("ControlWrite goroutine was stopped at %v.\n", time.Now())
+	}()
+	if p.Config.IsJSON {
+		for {
+			select {
+			case <-p.Pipes.StopControlWrite:
+				return
+			case v := <-p.Pipes.InboundJSON:
+				for _, socket := range p.OpenStack {
+					socket.FromPoolJSON <- v
+				}
+			default:
+				continue
+			}
+		}
+	} else {
+		for {
+			select {
+			case <-p.Pipes.StopControlWrite:
+				return
+			case v := <-p.Pipes.InboundBytes:
+				for _, socket := range p.OpenStack {
+					socket.FromPoolBytes <- v
+				}
+			default:
+				continue
+			}
+		}
+	}
+}
+
+// ControlShutdown method listens for Error Messages and dispatches shutdown messages
+func (p *SocketPool) ControlShutdown() {
+	defer func() {
+		log.Printf("ControlShutdown goroutine was stopped at %v.\n", time.Now())
+	}()
 	for {
 		select {
+		case <-p.Pipes.StopControlShutdown:
+			return
 		case e := <-p.Pipes.ErrorRead:
 			s := p.checkOpenStack(e.URL)
 			if s != nil {
@@ -150,13 +232,7 @@ func (p *SocketPool) Control() {
 				}
 			}
 		default:
-			if p.Config.IsJSON == true {
-				v := <-p.Pipes.FromSocketJSON
-				p.Pipes.OutboundJSON <- v
-			} else {
-				v := <-p.Pipes.FromSocketBytes
-				p.Pipes.OutboundBytes <- v
-			}
+			continue
 		}
 	}
 }
