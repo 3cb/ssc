79aeb577a2d87f8d15620a4c0ba05c882d569e9a
Marc C-B <marc.cbrooks@gmail.com>
2017-12-08 10:39:24 -0500

    move control methods to separate file

--- pool.go
+++ pool.go
@@ -90,138 +90,6 @@ func NewSocketPool(urls []string, config PoolConfig) (*SocketPool, error) {
 	return pool, nil
 }
 
-// Control method launches ControlShutdown(), ControlRead(), and ControlWrite()
-func (p *SocketPool) Control() {
-	go p.ControlShutdown()
-	if p.Config.IsReadable {
-		go p.ControlRead()
-	}
-	if p.Config.IsWritable {
-		go p.ControlWrite()
-	}
-}
-
-// ControlRead runs an infinite loop to take messages from websocket servers and send them to the outbound channel
-func (p *SocketPool) ControlRead() {
-	defer func() {
-		log.Printf("ControlRead goroutine was stopped at %v.\n", time.Now())
-	}()
-	if p.Config.IsJSON {
-		for {
-			select {
-			case <-p.Pipes.StopControlRead:
-				return
-			case v := <-p.Pipes.FromSocketJSON:
-				p.Pipes.OutboundJSON <- v
-			default:
-				continue
-			}
-		}
-	} else {
-		for {
-			select {
-			case <-p.Pipes.StopControlRead:
-				return
-			case v := <-p.Pipes.FromSocketBytes:
-				p.Pipes.OutboundBytes <- v
-			default:
-				continue
-			}
-		}
-	}
-}
-
-// ControlWrite runs an infinite loop to take messages from inbound channel and send to write goroutines
-func (p *SocketPool) ControlWrite() {
-	defer func() {
-		log.Printf("ControlWrite goroutine was stopped at %v.\n", time.Now())
-	}()
-	if p.Config.IsJSON {
-		for {
-			select {
-			case <-p.Pipes.StopControlWrite:
-				return
-			case v := <-p.Pipes.InboundJSON:
-				for _, socket := range p.OpenStack {
-					socket.FromPoolJSON <- v
-				}
-			default:
-				continue
-			}
-		}
-	} else {
-		for {
-			select {
-			case <-p.Pipes.StopControlWrite:
-				return
-			case v := <-p.Pipes.InboundBytes:
-				for _, socket := range p.OpenStack {
-					socket.FromPoolBytes <- v
-				}
-			default:
-				continue
-			}
-		}
-	}
-}
-
-// ControlShutdown method listens for Error Messages and dispatches shutdown messages
-func (p *SocketPool) ControlShutdown() {
-	defer func() {
-		log.Printf("ControlShutdown goroutine was stopped at %v.\n", time.Now())
-	}()
-	for {
-		select {
-		case <-p.Pipes.StopControlShutdown:
-			return
-		case e := <-p.Pipes.ErrorRead:
-			s := p.checkOpenStack(e.URL)
-			if s != nil {
-				rw, ok := p.ClosingQueue[e.URL]
-				if ok == false {
-					if s.IsWritable == true {
-						p.ClosingQueue[e.URL] = "read"
-						sock := p.OpenStack[e.URL]
-						sock.ShutdownWrite <- struct{}{}
-					} else {
-						s.ClosedAt = time.Now()
-						delete(p.OpenStack, e.URL)
-						p.ClosedStack[e.URL] = s
-					}
-				} else if ok == true && rw == "write" {
-					delete(p.ClosingQueue, e.URL)
-					s.ClosedAt = time.Now()
-					delete(p.OpenStack, e.URL)
-					p.ClosedStack[e.URL] = s
-				}
-			}
-		case e := <-p.Pipes.ErrorWrite:
-			s := p.checkOpenStack(e.URL)
-			if s != nil {
-				rw, ok := p.ClosingQueue[e.URL]
-				if ok == false {
-					if s.IsReadable == true {
-						p.ClosingQueue[e.URL] = "write"
-						sock := p.OpenStack[e.URL]
-						sock.ShutdownRead <- struct{}{}
-					} else {
-						s.ClosedAt = time.Now()
-						delete(p.OpenStack, e.URL)
-						p.ClosedStack[e.URL] = s
-					}
-				} else if ok == true && rw == "read" {
-					delete(p.ClosingQueue, e.URL)
-					s.ClosedAt = time.Now()
-					delete(p.OpenStack, e.URL)
-					p.ClosedStack[e.URL] = s
-				}
-			}
-		default:
-			continue
-		}
-	}
-}
-
 // AddSocket allows caller to add individual websocket connections to an existing pool of connections
 // New connection will adopt existing pool configuration(SocketPool.Config)
 func (p *SocketPool) AddSocket(url string) {
@@ -236,7 +104,13 @@ func (p *SocketPool) AddSocket(url string) {
 	}
 }
 
-// RemoveSocket allows caller to remove an individual websocmet connection from a SocketPool instance
+// ShutdownSocket allows caller to send shutdown signal to goroutines managing reads and writes to websocket
+// Goroutines will close websocket connection and then return
+func (p *SocketPool) ShutdownSocket(url string) {
+
+}
+
+// RemoveSocket allows caller to remove an individual websocket connection from a SocketPool instance
 // Function will send shutdown message and wait for confirmation from SocketPool.RemovalComplete channel
 // Method deletes Socket connection from ClosedStack before it exits
 func (p *SocketPool) RemoveSocket(url string) {
@@ -253,10 +127,14 @@ func (p *SocketPool) RemoveSocket(url string) {
 	closed, ok := p.ClosingQueue[url]
 	if ok && closed == "read" {
 		s = p.OpenStack[url]
-		s.ShutdownWrite <- struct{}{}
+		if s != nil {
+			s.ShutdownWrite <- struct{}{}
+		}
 	} else if ok && closed == "write" {
 		s = p.OpenStack[url]
-		s.ShutdownRead <- struct{}{}
+		if s != nil {
+			s.ShutdownRead <- struct{}{}
+		}
 	} else {
 		s.ShutdownRead <- struct{}{}
 		s.ShutdownWrite <- struct{}{}
